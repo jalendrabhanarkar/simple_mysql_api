3a01efb13c59b3291ea5994dab288e642f5c6cde
jalendrabhanarkar <jalendra@sapnasolutions.com>
2012-09-04 19:12:08 +0530

    empty

--- lib/simple_mysql_api.rb
+++ lib/simple_mysql_api.rb
@@ -3,7 +3,6 @@
 # require File.join(File.expand_path(File.dirname(__FILE__)), 'simple_mysql_api/has_simple_mysql_api')
 require "simple_mysql_api/version"
 require "simple_mysql_api/class_methods"
-require "simple_mysql_api/has_simple_mysql_api"
 module SimpleMysqlApi
   # Your code goes here...
 end
